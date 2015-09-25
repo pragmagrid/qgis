15bbe52b63237fac4b5552d068a8acc1408ccea3
nadyawilliams <nadya@sdsc.edu>
2015-09-24 18:18:10 -0700

    add prereq packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,40 @@
 #
 . /opt/rocks/share/devel/src/roll/etc/bootstrap-functions.sh
 
+# set qgis lib direcotry for dynamic linker
+echo "/opt/qgis/bli" > /etc/ld.so.conf.d/qgis.conf
+/sbin/ldconfig
+
 # qt4 and its dependencies 
 compile wxWidgets
 install qgis-wxWidgets
+/sbin/ldconfig
 
 compile wxPython
 install qgis-wxPython
+/sbin/ldconfig
 
 compile qt
 install qgis-qt
+/sbin/ldconfig
 
 compile sip
 install qgis-sip
+/sbin/ldconfig
 
 compile pyqt4
 install qgis-pyqt4
+/sbin/ldconfig
+
+# compile freexl, need for spatialite
+compile freexl
+install qgis-freexl
+/sbin/ldconfig
+
+compile spatialite
+install qgis-spatialite
+/sbin/ldconfig
+
+compile qscintilla2
+install qgis-qscintilla2
+/sbin/ldconfig
